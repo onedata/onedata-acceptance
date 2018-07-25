3acd135cde7ed1df2d1791ddb23ea48a81c44089
Lukasz Niemiec <lukasz19281@gmail.com>
2018-07-25 17:09:24 +0200

    VFS-4715 Added simple groups tests
    Former-commit-id: 89159cd7a43fd12e42be47e168a1587cecebf3d6

--- tests/gui/features/onezone/groups.feature
+++ tests/gui/features/onezone/groups.feature
@@ -20,20 +20,39 @@ Feature: Basic management of groups in Onezone GUI
 
 
   Scenario: User creates group
-    When user of browser clicks on the CREATE button in "groups" Onezone panel
+    When user of browser clicks on the create button in "groups" Onezone panel
     And user of browser writes "newgroup" into group name text field
     And user of browser confirms group name
-    Then user of browser sees group "newgroup"
+    And user of browser do see group "newgroup" on groups list
+   
 
-
-  Scenario: User renames group
+  Scenario: User creates and renames group
     # create new group
-    When user of browser clicks on the CREATE button in "groups" Onezone panel
+    When user of browser clicks on the create button in "groups" Onezone panel
     And user of browser writes "newgroup" into group name text field
     And user of browser confirms group name
 
     # rename group
-    And user of browser clicks on RENAME button in "newgroup" group options menu
+    And user of browser clicks on "newgroup" group menu button
+    And user of browser clicks on rename button in active menu
+    And user of browser writes "newname" into rename group text field
+    And user of browser confirms group rename
+    
+    Then user of browser do see group "newname" on groups list
+    And user of browser dont see group "newgroup" on groups list
+
+
+#  Scenario: User joins group
+    #TODO
+
+
+#  Scenario: User tries to join nonexisting group
+    #TODO
+
+
+#  Scenario: User tries to create unnamed group
+#    When user of browser clicks on the create button in "groups" Onezone panel
+#    And user of browser 
     
     
 
--- tests/gui/steps/onezone/groups.py
+++ tests/gui/steps/onezone/groups.py
@@ -9,9 +9,68 @@ __license__ = ("This software is released under the MIT license cited in "
 
 
 from pytest_bdd import parsers, when, then
+from tests.utils.acceptance_utils import wt
 from tests.gui.steps.common import *
 
-@when(parsers.parse('user of {browser_id} clicks on the join button in "{panel}" Onezone panel'))
-@then(parsers.parse('user of {browser_id} clicks on the join button in "{panel}" Onezone panel'))
-def click_join_in_panel(selenium, browser_id, panel, oz_page):
-    oz_page(selenium[browser_id])[panel].join_group.click()
+@wt(parsers.re('user of (?P<browser_id>.*) clicks on the '
+               '(?P<operation>create|join) button in '
+               '"(?P<panel>.*)" Onezone panel'))
+def click_button_in_panel(selenium, browser_id, operation, panel, oz_page):
+    if operation == 'create':
+        oz_page(selenium[browser_id])[panel].create_group.click()
+    else:
+        oz_page(selenium[browser_id])[panel].join_group.click()
+
+
+@wt(parsers.parse('user of {browser_id} writes "{text}" '
+                  'into group name text field'))
+def input_new_group_name(selenium, browser_id, text, oz_page):
+    oz_page(selenium[browser_id])['groups'].input_box.value = text
+
+
+@wt(parsers.parse('user of {browser_id} confirms group name'))
+def confirm_group_name(selenium, browser_id, oz_page):
+    oz_page(selenium[browser_id])['groups'].input_box.confirm.click()
+
+
+def _find_groups(page, group):
+    return filter(lambda g : g.name == group, page.elements_list)
+
+
+@wt(parsers.re('user of (?P<browser_id>.*) (?P<option>do|dont) '
+               'see group "(?P<group>.*)" on groups list'))
+def check_group_existance(selenium, browser_id, option, group, oz_page):
+    groups_count = len(_find_groups(oz_page(selenium[browser_id])['groups'], group))
+    if option == 'do':
+        assert groups_count > 0, "no such group exists"
+    else:
+        assert groups_count == 0, "such group exists"
+
+
+@wt(parsers.parse('user of {browser_id} clicks on "{group}" group menu button'))
+def open_group_menu(selenium, browser_id, group, oz_page, tmp_memory):
+    groups = oz_page(selenium[browser_id])['groups']
+    tmp_memory['group'] = groups
+    groups.elements_list[group].menu.click()
+
+@wt(parsers.re('user of (?P<browser_id>.*) clicks on '
+               '(?P<option>rename|join|leave|delete) button in active menu'))
+def click_on_menu(selenium, browser_id, option, oz_page, tmp_memory):
+    button_id = {'rename': 0, 'join': 1, 'leave': 2, 'delete': 3}[option]
+    groups = tmp_memory['group'] #oz_page(selenium[browser_id])['groups']
+    groups.group_menu[button_id].click()
+
+@wt(parsers.parse('user of {browser_id} writes '
+                  '"{text}" into rename group text field'))
+def input_new_name(selenium, browser_id, text, oz_page):
+    groups = oz_page(selenium[browser_id])['groups']
+    groups.elements_list[''].input_field = text
+    
+@wt(parsers.parse('user of {browser_id} confirms group rename'))
+def confirm_rename(selenium, browser_id, oz_page):
+    groups = oz_page(selenium[browser_id])['groups']
+    groups.elements_list[''].confirm_button.click()
+        
+
+
+
