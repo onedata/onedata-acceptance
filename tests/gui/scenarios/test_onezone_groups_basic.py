cd6c4b351b83ff738423b68cb552ac898efab258
Lukasz Niemiec <lukasz19281@gmail.com>
2018-07-27 09:27:27 +0200

    VFS-4715 Moved groups tests to separate file
    Former-commit-id: 2025c40123e07d77232329b1d28da6a988c636ce

--- tests/gui/scenarios/test_onezone_groups_basic.py
+++ tests/gui/scenarios/test_onezone_groups_basic.py
@@ -1,9 +1,9 @@
 """This module contains tests suite for basic operations using
-Onezone GUI and single browser instance.
+Onezone Groups GUI and single browser instance.
 """
 
-__author__ = "Bartosz Walkowicz"
-__copyright__ = "Copyright (C) 2017 ACK CYFRONET AGH"
+__author__ = "Lukasz Niemiec"
+__copyright__ = "Copyright (C) 2018 ACK CYFRONET AGH"
 __license__ = ("This software is released under the MIT license cited in "
                "LICENSE.txt")
 
@@ -35,6 +35,7 @@ from tests.gui.steps.onezone.access_tokens import *
 from tests.gui.steps.onezone.data_space_management import *
 from tests.gui.steps.onezone.providers import *
 from tests.gui.steps.onezone.manage_account import *
+from tests.gui.steps.onezone.groups import *
 
 from tests.gui.steps.oneprovider.common import *
 from tests.gui.steps.oneprovider.data_tab import *
@@ -53,15 +54,5 @@ def screens():
     return [0]
 
 
-@pytest.mark.skip('it collides with deregister provider test, '
-                  'so skip it on getting started for now)')
-@scenario('../features/onezone/providers.feature',
-          'User sees that when no provider is working appropriate msg is shown')
-def test_user_sees_that_when_no_provider_is_working_appropriate_msg_is_shown():
-    pass
+scenarios('../features/onezone/groups.feature')
 
-
-scenarios('../features/onezone/access_tokens.feature')
-scenarios('../features/onezone/login_page.feature')
-scenarios('../features/onezone/providers.feature')
-scenarios('../features/onezone/user_alias.feature')
