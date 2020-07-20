1788f0833cbf0f81eb4ff19e55cfd318acbd79f4
kwlk <emiliakwolek@poczta.fm>
2020-07-20 14:53:18 +0200

    VFS-6565 download and evict test
    Former-commit-id: 9bbf0824034b919d3707e475ba1748d5ea61c364

--- tests/gui/meta_steps/oneprovider/transfers.py
+++ tests/gui/meta_steps/oneprovider/transfers.py
@@ -10,9 +10,14 @@ __license__ = ("This software is released under the MIT license cited in "
 from pytest_bdd import parsers
 
 from tests.gui.conftest import WAIT_FRONTEND
+from tests.gui.steps.modal import wt_wait_for_modal_to_appear, \
+    wt_click_on_confirmation_btn_in_modal
 from tests.gui.steps.oneprovider.data_tab import (
     click_choose_other_oneprovider_on_file_browser,
     choose_provider_in_file_browser, check_current_provider_in_space)
+from tests.gui.steps.oneprovider.file_browser import \
+    click_menu_for_elem_in_file_browser, \
+    click_option_in_data_row_menu_in_file_browser
 from tests.gui.steps.oneprovider.transfers import (
     wait_for_transfers_page_to_load)
 from tests.gui.steps.onezone.spaces import (
@@ -41,3 +46,27 @@ def open_transfers_page(selenium, browser_id, provider, space, hosts, oz_page,
 
     wait_for_transfers_page_to_load(selenium, browser_id, op_container)
 
+
+@wt(parsers.parse('user of {browser_id} evicts file "{file_name}" from provider'
+                  ' {provider}'))
+@repeat_failed(timeout=WAIT_FRONTEND)
+def evict_file(selenium, browser_id, provider, popups, file_name, tmp_memory,
+               modals, hosts):
+    option = 'Data distribution'
+    modal_name = 'Data distribution'
+
+    click_menu_for_elem_in_file_browser(browser_id, file_name, tmp_memory)
+    click_option_in_data_row_menu_in_file_browser(selenium, browser_id,
+                                                  option, modals)
+    wt_wait_for_modal_to_appear(selenium, browser_id, modal_name, tmp_memory)
+    menu_option = 'Evict'
+    driver = selenium[browser_id]
+    provider_name = hosts[provider]['name']
+    # import pdb
+    # pdb.set_trace()
+
+    data_distribution_modal = modals(driver).data_distribution
+    data_distribution_modal.providers[provider_name].menu_button()
+    popups(driver).data_distribution_menu.menu[menu_option]()
+    wt_click_on_confirmation_btn_in_modal(selenium, browser_id, 'Close',
+                                          tmp_memory)
