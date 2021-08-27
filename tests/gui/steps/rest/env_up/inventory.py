c96379df74ced86a8eca42b35bb5a69f7950860c
KasiaS999 <ksuch999@gmail.com>
2021-08-27 10:52:58 +0200

    VFS-8254 spaces and automation effective privileges tests
    Former-commit-id: af496973d42e4acb70d9159d7868d8d2bb90909a

--- tests/gui/steps/rest/env_up/inventory.py
+++ tests/gui/steps/rest/env_up/inventory.py
@@ -12,18 +12,32 @@ import yaml
 
 from tests import OZ_REST_PORT
 from tests.utils.bdd_utils import given, parsers
-from tests.utils.rest_utils import (http_post, get_zone_rest_path)
+from tests.utils.rest_utils import (http_post, get_zone_rest_path, http_put)
 
 
 @given(parsers.parse('initial inventories configuration in "{zone_name}" '
                      'Onezone service:\n{config}'))
-def inventories_creation(config, hosts, users, zone_name):
+def inventories_creation(config, hosts, users, zone_name, admin_credentials,
+                         groups):
     """Create and configure inventories according to given config.
 
     Config format given in yaml is as follow:
 
         inventory_name_1:
             owner: user_name
+
+            [users]:                        ---> optional
+                - user_name_2
+                - user_name_3:
+                    [privileges]:           ---> optional
+                        - privilege_1
+                        - privilege_2
+            [groups]:                       ---> optional
+                - group_name_1:
+                    [privileges]:           ---> optional
+                        - privilege_1
+                        - privilege_2
+                - group_name_2
         inventory_name_2:
             ...
 
@@ -31,13 +45,20 @@ def inventories_creation(config, hosts, users, zone_name):
 
         inventory1:
             owner: user3
+            users:
+                - user2
+                - user1
+            groups:
+                - group1
         inventory2:
             owner: user1
     """
-    _inventories_creation(config, hosts, users, zone_name)
+    _inventories_creation(config, hosts, users, zone_name, admin_credentials,
+                          groups)
 
 
-def _inventories_creation(config, hosts, users, zone_name):
+def _inventories_creation(config, hosts, users, zone_name, admin_credentials,
+                          groups):
     zone_hostname = hosts[zone_name]['hostname']
 
     config = yaml.load(config)
@@ -45,8 +66,31 @@ def _inventories_creation(config, hosts, users, zone_name):
     for inventory_name, description in config.items():
         owner = users[description['owner']]
 
-        _create_inventory(zone_hostname, owner.username, owner.password,
-                          inventory_name)
+        inventory_id = _create_inventory(zone_hostname, owner.username,
+                                         owner.password, inventory_name)
+        for user in description.get('users', {}):
+            try:
+                [(user, options)] = user.items()
+            except AttributeError:
+                privileges = None
+            else:
+                privileges = options['privileges']
+
+            _add_user_to_inventory(zone_hostname, admin_credentials,
+                                   inventory_id, users[user].id, privileges)
+
+        for group in description.get('groups', {}):
+            try:
+                [(group, options)] = group.items()
+            except AttributeError:
+                privileges = None
+            else:
+                privileges = options['privileges']
+
+            group_id = groups[group]
+
+            _add_group_to_inventory(zone_hostname, admin_credentials,
+                                    inventory_id, group_id, privileges)
 
 
 def _create_inventory(zone_hostname, owner_username, owner_password,
@@ -59,3 +103,32 @@ def _create_inventory(zone_hostname, owner_username, owner_password,
                          data=inventory_properties)
 
     return response.headers['location'].split('/')[-1]
+
+
+def _add_user_to_inventory(zone_hostname, admin_credentials,
+                           inventory_id, user_id, privileges):
+    if privileges:
+        data = json.dumps({'privileges': privileges})
+    else:
+        data = None
+
+    http_put(ip=zone_hostname, port=OZ_REST_PORT,
+             path=get_zone_rest_path('atm_inventories', inventory_id, 'users',
+                                     user_id),
+             auth=(admin_credentials.username, admin_credentials.password),
+             data=data)
+
+
+def _add_group_to_inventory(zone_hostname, admin_credentials, inventory_id,
+                            group_id, privileges):
+    if privileges:
+        data = json.dumps({'privileges': privileges})
+    else:
+        data = None
+
+    http_put(ip=zone_hostname, port=OZ_REST_PORT,
+             path=get_zone_rest_path('atm_inventories', inventory_id,
+                                     'groups', group_id),
+             auth=(admin_credentials.username, admin_credentials.password),
+             data=data)
+
