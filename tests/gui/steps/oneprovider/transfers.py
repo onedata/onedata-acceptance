cba21ea207422bd85347324c21e8bdc819ec3b0e
rwidzisz <rafal.widziszewski@gmail.com>
2021-09-15 10:31:31 +0200

    VFS-8323 migration modal dropdown menu fix 1
    Former-commit-id: a744d33fe437146f912eb5e28d576dbf72f67edf

--- tests/gui/steps/oneprovider/transfers.py
+++ tests/gui/steps/oneprovider/transfers.py
@@ -13,10 +13,12 @@ import yaml
 from selenium.common.exceptions import StaleElementReferenceException
 
 from tests.gui.steps.common.miscellaneous import switch_to_iframe
+from tests.gui.steps.modal import wt_wait_for_modal_to_appear
 from tests.gui.utils.common.modals import Modals as modals
 from tests.utils.utils import repeat_failed
 from tests.utils.bdd_utils import wt, parsers
 from tests.gui.conftest import WAIT_FRONTEND, WAIT_BACKEND
+from selenium.webdriver.support.ui import WebDriverWait as Wait
 
 
 def _assert_transfer(transfer, item_type, desc, sufix, hosts):
@@ -94,6 +96,23 @@ def _expand_dropdown_in_migrate_record(driver):
     data_distribution_modal.migrate.expand_dropdown()
 
 
+@repeat_failed(timeout=WAIT_FRONTEND)
+def _wait_for_dropdown_menu_to_expand(driver):
+    data_distribution_modal = modals(driver).data_distribution
+
+    def check_dropdown_menu():
+        dropdown_status = data_distribution_modal.migrate._toggle.get_attribute(
+            'aria-expanded')
+        dropdown_status = str(dropdown_status)
+
+        return dropdown_status
+
+    return Wait(driver, WAIT_BACKEND).until(
+        check_dropdown_menu() == 'true',
+        message='Dropdownmenu has not expanded'
+    )
+
+
 @wt(parsers.re('user of (?P<browser_id>.*) migrates selected item from '
                'provider "(?P<source>.*)" to provider "(?P<target>.*)"'))
 def migrate_item(selenium, browser_id, source, target, hosts, popups):
@@ -108,6 +127,7 @@ def migrate_item(selenium, browser_id, source, target, hosts, popups):
     popups(driver).data_distribution_popup.menu[menu_option]()
 
     _expand_dropdown_in_migrate_record(driver)
+    _wait_for_dropdown_menu_to_expand(driver)
     modals(driver).dropdown.options[target_name].click()
 
     data_distribution_modal.migrate.migrate_button()
