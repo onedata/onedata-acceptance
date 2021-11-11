0c0093ec10a9fb5249880e22f128cc2001c8f54c
rwidzisz <rafal.widziszewski@gmail.com>
2021-11-11 19:42:54 +0100

    VFS-8308 scenario added to suit
    Former-commit-id: f9b3a98c0f9322f4792c7aec6487e2b06ef38470

--- tests/gui/scenarios/test_oneprovider_upload.py
+++ tests/gui/scenarios/test_oneprovider_upload.py
@@ -114,3 +114,9 @@ def test_user_successfully_uploads_2_gb_file_stress_test():
 @scenario('User successfully uploads 300 files (stress test)')
 def test_user_successfully_uploads_300_files_stress_test():
     pass
+
+
+@skip_if_not_chrome
+@scenario('User with weak connection uploads one large file')
+def test_user_with_weak_connection_uploads_one_large_file():
+    pass
