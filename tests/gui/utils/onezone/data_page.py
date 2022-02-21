c015f8b22c35d596e24a8de29281ad8dc5ed748f
plgagneska <agawarchol15@gmail.com>
2022-02-21 12:58:59 +0100

    VFS-9031 Change current provider selector
    Former-commit-id: 135e5e5d94baeea8dda3f8aea5ec1e61d96bb4d2

--- tests/gui/utils/onezone/data_page.py
+++ tests/gui/utils/onezone/data_page.py
@@ -215,7 +215,8 @@ class DataPage(GenericPage):
 
     tab_name = Label('.header-row')
 
-    current_provider = Label('.current-oneprovider-name')
+    current_provider = Label('.current-oneprovider-bar .oneprovider-name '
+                             '.tab-name')
     providers = WebItemsSequence('.provider-online', cls=_Provider)
     choose_other_provider = Button('.choose-oneprovider-link')
     error_header = Label('.content-info-content-container h1')
