a9a527334437ae4e4ca9d0ace9617286f00cc847
plgagneska <agawarchol15@gmail.com>
2024-04-22 12:56:04 +0200

    VFS-11536 Fix archive tests

--- tests/gui/utils/oneprovider/archive_file_browser/data_row.py
+++ tests/gui/utils/oneprovider/archive_file_browser/data_row.py
@@ -26,10 +26,8 @@ class DataRow(PageObject, BrowserRow):
     _status_tag = WebElement('.file-status-tag')
 
     def click_and_enter(self):
-        if self.is_any_tag_visible():
-            ActionChains(self.driver).click(self.clickable_field).perform()
-        else:
-            ActionChains(self.driver).click(self.web_elem).perform()
+        time.sleep(0.1)
+        ActionChains(self.driver).click(self.clickable_field).perform()
         self.wait_for_selected()
         ActionChains(self.driver).key_down(Keys.ENTER).perform()
 
