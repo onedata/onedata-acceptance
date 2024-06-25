31bb3d92c01650099cb2675c869b15b9171be907
plgagneska <agawarchol15@gmail.com>
2024-06-25 15:19:20 +0200

    VFS-12052 Remove skipStorageDetection step

--- tests/utils/entities_setup/storage.py
+++ tests/utils/entities_setup/storage.py
@@ -28,7 +28,6 @@ def create_storage(hosts, host, config, onepanel_credentials, name):
         importedStorage: true/false
         lumaFeed: "auto"/"local"/"external"
         simulatedFilesystemParameters: filesystem_parameters      ---> optional
-        skipStorageDetection: true/false
         storagePathType: "flat"/"canonical"
         minLatency: min_latency                                   ---> optional
         maxLatency: max_latency                                   ---> optional
