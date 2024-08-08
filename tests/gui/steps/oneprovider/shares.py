6d864d15a23f3ea93829b1795943f7639e395aec
jakub <jakub@pilch.hub.pl>
2024-08-08 11:45:45 +0200

    VFS-10453 Resolve PR task

--- tests/gui/steps/oneprovider/shares.py
+++ tests/gui/steps/oneprovider/shares.py
@@ -251,33 +251,17 @@ def save_description_changes(selenium, browser_id, op_container):
     op_container(driver).shares_page.save_description()
 
 
-@wt(parsers.parse('user of {browser_id} sees share "{share_name}" '
-                  'from "{space_name}" space'))
-@repeat_failed(timeout=WAIT_FRONTEND)
-def assert_space_name_matches_share_name(selenium, browser_id,
-                                                    oz_page, share_name,
-                                                    space_name):
-    shares_list = oz_page(selenium[browser_id])['shares'].shares_list
-    share_names_list = {share.name for share in shares_list}
-    assert share_name in share_names_list, (f'Share {share_name} not in '
-                                f'shares sidebar')
-    found_space_name = shares_list[share_name].space_name
-    assert space_name == found_space_name, (f'Space name for share '
-                                            f'"{share_name}": '
-                                            f'"{found_space_name} does not '
-                                            f'match expected: "{space_name}')
-
-
 @wt(parsers.parse('user of {browser_id} sees space name "{space_name}" of '
                   '"{share_name}" share in shares list in the sidebar'))
 @repeat_failed(timeout=WAIT_FRONTEND)
-def assert_space_name_matches_share_name(selenium, browser_id,
-                                                    oz_page, share_name,
-                                                    space_name):
+def assert_space_name_for_share_matches_expected(selenium, browser_id, oz_page,
+                                                 share_name, space_name):
     shares_list = oz_page(selenium[browser_id])['shares'].shares_list
     share_names_list = {share.name for share in shares_list}
+
     if share_name not in share_names_list:
         raise RuntimeError(f'Share {share_name} not in shares sidebar')
+
     found_space_name = shares_list[share_name].space_name
     assert space_name == found_space_name, (f'Space name for share '
                                             f'"{share_name}": '
@@ -288,8 +272,9 @@ def assert_space_name_matches_share_name(selenium, browser_id,
 @wt(parsers.parse('user of {browser_id} sees share name "{share_name}" in the '
                   'shares list in the sidebar'))
 @repeat_failed(timeout=WAIT_FRONTEND)
-def assert_share_name_in_shares_list(selenium, browser_id, oz_page, share_name):
+def assert_share_name_in_shares_sidebar(selenium, browser_id, oz_page, share_name):
     shares_list = oz_page(selenium[browser_id])['shares'].shares_list
     share_names_list = {share.name for share in shares_list}
+
     assert share_name in share_names_list, (f'Share {share_name} not in '
-                                f'shares sidebar')
+                                            f'shares sidebar')
