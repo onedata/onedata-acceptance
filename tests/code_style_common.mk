35df6f0b35bdafe3cb04af119ec798a3732f2b0a
wojciech <wojciech05521@gmail.com>
2024-10-11 16:58:19 +0200

    VFS-12190 updated code style make file

--- tests/code_style_common.mk
+++ tests/code_style_common.mk
@@ -1,4 +1,6 @@
 STATIC_ANALYSER_IMAGE := "docker.onedata.org/python_static_analyser:v8"
+GUI_FILES := gui/steps gui/meta_steps gui/utils gui/__init__.py
+FILES_TO_FORMAT := gui/steps gui/meta_steps gui/utils gui/__init__.py gui/conftest.py
 
 .PHONY: format
 
@@ -7,12 +9,12 @@ STATIC_ANALYSER_IMAGE := "docker.onedata.org/python_static_analyser:v8"
 ##
 
 format:
-	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) isort gui/steps gui/meta_steps gui/scenarios gui/utils --settings-file configs/.pyproject.toml
-	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) black gui/steps gui/meta_steps gui/scenarios gui/utils --config configs/.pyproject.toml
+	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) isort $(FILES_TO_FORMAT) --settings-file configs/.pyproject.toml
+	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) black $(FILES_TO_FORMAT) --config configs/.pyproject.toml
 
 
 black-check:
-	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) black gui/steps gui/meta_steps gui/scenarios gui/utils --check --config configs/.pyproject.toml || \
+	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) black $(FILES_TO_FORMAT) --check --config configs/.pyproject.toml || \
 	 (echo "Code failed Black format checking. Please run 'make format' before commiting your changes. "; exit 1)
 
 	
@@ -21,5 +23,7 @@ black-check:
 ##
 
 static-analysis:	
-	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) pylint gui/steps gui/meta_steps gui/utils --recursive=y --rcfile=configs/.pylintrc
+	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) pylint $(GUI_FILES) --recursive=y --rcfile=configs/.pylintrc
+	docker run --rm -i -v `pwd`:`pwd` -w `pwd`  $(STATIC_ANALYSER_IMAGE) pylint gui/conftest.py --recursive=y \
+	--disable=redefined-outer-name,import-outside-toplevel,protected-access,unused-argument --rcfile=configs/.pylintrc
 
