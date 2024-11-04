7e9fe31cf3952eed6b860f4eee043f000adf1d25
wojciech <wojciech05521@gmail.com>
2024-11-04 14:37:47 +0100

    VFS-12371 restored qos tag css sel, removed try,except

--- tests/gui/meta_steps/oneprovider/data.py
+++ tests/gui/meta_steps/oneprovider/data.py
@@ -1098,11 +1098,7 @@ def get_file_id_from_details_modal(
     )
     wt_wait_for_modal_to_appear(selenium, browser_id, modal_name, tmp_memory)
     click_modal_button(selenium, browser_id, "file_id", modal_name, modals)
-
-    try:
-        click_modal_button(selenium, browser_id, "x", modal_name, modals)
-    except RuntimeError:
-        pass
+    click_modal_button(selenium, browser_id, "x", modal_name, modals)
 
     return clipboard.paste(display=displays[browser_id])
 
