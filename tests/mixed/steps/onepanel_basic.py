2879d1b34cb7fa5ed13ae6724857bfbef757a941
wojciech <wojciech05521@gmail.com>
2025-01-28 14:09:33 +0100

    VFS-12602 added formating

--- tests/mixed/steps/onepanel_basic.py
+++ tests/mixed/steps/onepanel_basic.py
@@ -22,6 +22,7 @@ from tests.gui.meta_steps.onepanel.spaces import (
     configure_sync_parameters_for_space_in_op_panel_gui,
     copy_id_of_space_gui,
     revoke_space_support_in_op_panel_using_gui,
+    run_scan_and_wait_till_finished,
     support_space_in_op_panel_using_gui,
 )
 from tests.gui.meta_steps.oneprovider.data import assert_space_content_in_op_gui
@@ -35,7 +36,6 @@ from tests.gui.meta_steps.onezone.spaces import (
     assert_space_is_supported_by_provider_in_oz_gui,
     request_space_support_using_gui,
 )
-from tests.gui.meta_steps.onepanel.spaces import run_scan_and_wait_till_finished
 from tests.gui.steps.common.docker import (
     wt_cp_files_to_dir_in_storage_mount_point,
     wt_cp_files_to_dst_path_in_space,
@@ -72,7 +72,10 @@ from tests.mixed.steps.rest.onezone.space_management import (
 )
 from tests.mixed.utils.common import NoSuchClientException
 from tests.utils.bdd_utils import parsers, wt
-from tests.utils.entities_setup.spaces import force_start_storage_scan, wait_for_storage_scan_to_finish
+from tests.utils.entities_setup.spaces import (
+    force_start_storage_scan,
+    wait_for_storage_scan_to_finish,
+)
 from tests.utils.utils import repeat_failed
 
 
@@ -939,7 +942,15 @@ def force_start_storage_import_scan(
     )
 )
 def force_start_and_wait_to_finish_storage_import_scan(
-    client, user, provider, space, spaces, hosts, onepanel_credentials, selenium, onepanel
+    client,
+    user,
+    provider,
+    space,
+    spaces,
+    hosts,
+    onepanel_credentials,
+    selenium,
+    onepanel,
 ):
     if client.lower() == "rest":
         space_id = spaces[space]
