54dd90ba0040929a7e60bb163058593cd45b3dff
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2025-07-30 15:24:57 +0200

    VFS-10395 Added new import for test file

--- tests/gui/scenarios/test_oneprovider_datasets.py
+++ tests/gui/scenarios/test_oneprovider_datasets.py
@@ -52,6 +52,7 @@ from tests.gui.steps.onezone.providers import *
 from tests.gui.steps.onezone.spaces import *
 from tests.gui.steps.onezone.tokens import *
 from tests.gui.steps.onezone.user_full_name import *
+from tests.mixed.steps.dataset import *
 from tests.utils.acceptance_utils import *
 from tests.utils.entities_setup.groups import *
 from tests.utils.entities_setup.spaces import *
