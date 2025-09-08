e5cbecfdd0de438c196f1611b975a4b0625cd91b
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2025-09-08 13:43:10 +0200

    VFS-11052 Minor changes

--- tests/gui/scenarios/test_oneprovider_acl_dir_privileges_create.py
+++ tests/gui/scenarios/test_oneprovider_acl_dir_privileges_create.py
@@ -28,6 +28,7 @@ from tests.gui.steps.onepanel.deployment import *
 from tests.gui.steps.onepanel.nodes import *
 from tests.gui.steps.onepanel.spaces import *
 from tests.gui.steps.oneprovider.browser import *
+from tests.gui.steps.oneprovider.file_browser import *
 from tests.gui.steps.oneprovider.public_shares import *
 from tests.gui.steps.onezone.providers import *
 from tests.utils.acceptance_utils import *
