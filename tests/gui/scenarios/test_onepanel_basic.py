5d88bef460382a0e9390b40026ee96c270b24496
wojciech <wojciech05521@gmail.com>
2025-10-06 14:03:00 +0200

    VFS-12902 code refactor

--- tests/gui/scenarios/test_onepanel_basic.py
+++ tests/gui/scenarios/test_onepanel_basic.py
@@ -59,8 +59,6 @@ from tests.gui.steps.onezone.providers import *
 from tests.gui.steps.onezone.spaces import *
 from tests.gui.steps.onezone.tokens import *
 from tests.gui.steps.onezone.user_full_name import *
-from tests.gui.steps.rest.s3_service_boto3 import *
-from tests.mixed.steps.tokens_basic import *
 from tests.utils.acceptance_utils import *
 from tests.utils.entities_setup.groups import *
 from tests.utils.entities_setup.spaces import *
