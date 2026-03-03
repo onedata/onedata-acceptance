7b2e379e60da96e5d30bb24045d73100726d9951
wojciech <wojciech05521@gmail.com>
2026-03-03 14:06:42 +0100

    VFS-13317 changed css selectors in archive header columns

--- tests/gui/utils/oneprovider/archive_browser/__init__.py
+++ tests/gui/utils/oneprovider/archive_browser/__init__.py
@@ -7,7 +7,7 @@ __license__ = "This software is released under the MIT license cited in LICENSE.
 from functools import partial
 
 from tests.gui.utils.core.base import PageObject
-from tests.gui.utils.core.web_elements import Button, Label, WebElement, WebItem
+from tests.gui.utils.core.web_elements import Button, Label, WebElement, WebItem, WebItemsSequence
 
 from ..browser import Browser
 from .data_row import DataRow
@@ -19,7 +19,7 @@ class ArchiveColumnHeader(PageObject):
 
 class _ArchiveBrowser(Browser):
     row_cls = DataRow
-    column_header_cls = ArchiveColumnHeader
+    column_headers = WebItemsSequence(".archive-table-head-row .fb-table-secondary-col", cls=ArchiveColumnHeader)
 
     create_archive = Button(".hidden-xs .toolbar-buttons .oneicon-browser-archive-add")
     create_archive_elem = WebElement(
