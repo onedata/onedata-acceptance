cd7a2301d7c1c50b2b9eb93f33148c20ae44d9ad
wojciech <wojciech05521@gmail.com>
2026-03-24 12:09:35 +0100

    VFS-13416 fixed tests

--- tests/gui/meta_steps/onezone/clusters.py
+++ tests/gui/meta_steps/onezone/clusters.py
@@ -98,7 +98,7 @@ def change_privilege_config_in_cluster(
     cluster = "oneprovider-1"
 
     wt_click_on_subitem_for_item(
-        selenium, browser_id, option, "Members", cluster, hosts
+        selenium, browser_id, "CLUSTERS", "Members", cluster, hosts
     )
     click_element_in_members_list(selenium, browser_id, user_name, where, list_type)
     see_privileges_for_member(selenium, browser_id, where, member_type, user_name)
