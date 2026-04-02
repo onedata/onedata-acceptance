869fde2a3234a5a7c0a5fecb3d1b2b5088f17c68
Michał Stanisz <mch.stanisz@gmail.com>
2026-04-02 09:27:21 +0200

    Increased env timeout in upgrade tests

--- tests/utils/environment_utils.py
+++ tests/utils/environment_utils.py
@@ -509,7 +509,7 @@ def verify_env_ready(admin_user, hosts):
     ready = False
     start = time.time()
     while not ready:
-        if time.time() - start > ENV_READY_TIMEOUT_SECONDS:
+        if time.time() - start > 2*ENV_READY_TIMEOUT_SECONDS:
             raise RuntimeError("Environment not ready after upgrade")
         time.sleep(1)
         try:
