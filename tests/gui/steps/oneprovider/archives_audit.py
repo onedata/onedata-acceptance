f213791544f24dadab64237605c4fea5d771c733
Mateusz Zając <magros.thornrime@gmail.com>
2026-05-11 20:48:43 +0200

    VFS-13557 Update logs in  non empty column content assertion

--- tests/gui/steps/oneprovider/archives_audit.py
+++ tests/gui/steps/oneprovider/archives_audit.py
@@ -51,9 +51,14 @@ def assert_number_of_first_non_empty_column_content(
                 ), f"there is empty {field} field: {elem} in archive audit log"
 
     checked_elems = _scroll_and_check_condition(browser_id, selenium, condition2)
+    unique_elems = list(set(checked_elems))
     assert (
         len(checked_elems) == number
-    ), f"there is {len(checked_elems)} entries instead of {number} in archive audit log"
+    ), f"""There is {len(checked_elems)} entries instead of {number} 
+    in archive audit log.\n\n
+    Number of unique entries: {len(unique_elems)}\n\n
+    Entries: {sorted(checked_elems)}\n\n
+    Unique entries: {sorted(unique_elems)}"""
 
 
 @wt(
