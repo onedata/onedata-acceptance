f86b3360f3cb491aa2b1287c1a4028c4915bef82
wojciech <wojciech05521@gmail.com>
2026-06-11 01:17:29 +0200

    VFS-13494 added waiting for sliding animation in atm

--- tests/gui/steps/onezone/automation/workflow_creation.py
+++ tests/gui/steps/onezone/automation/workflow_creation.py
@@ -9,6 +9,7 @@ import json
 import time
 
 from tests.gui.conftest import WAIT_FRONTEND
+from tests.gui.steps.common.common import wait_for_sliding_panel_to_stop_moving
 from tests.gui.steps.common.miscellaneous import press_backspace_on_active_element
 from tests.gui.steps.modals.modal import wt_wait_for_modal_to_appear
 from tests.gui.steps.onezone.automation.automation_basic import collapse_revision_list
@@ -29,6 +30,9 @@ from tests.utils.utils import repeat_failed
 def click_add_new_button_in_menu_bar(selenium, browser_id, option):
     driver = selenium[browser_id]
     getattr(OZLoggedIn(driver)["automation"].main_page, transform(option)).click()
+    wait_for_sliding_panel_to_stop_moving(
+        driver, WAIT_FRONTEND, '[data-one-carousel-slide-id="editor"]'
+    )
 
 
 @wt(
