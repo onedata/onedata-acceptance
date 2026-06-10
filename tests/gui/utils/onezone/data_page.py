93c60590c53f633100942045d026d88bb3702de5
Łukasz Opioła <ymopiola@cyfronet.pl>
2026-06-10 10:20:42 +0200

    VFS-13444 Revert "VFS-13605 Fix space header selector"
    This reverts commit 4693b321d8b14c5178d4dc336ab8b7cdb02af260.

--- tests/gui/utils/onezone/data_page.py
+++ tests/gui/utils/onezone/data_page.py
@@ -224,9 +224,7 @@ class DataPage(GenericPage):
     marketplace_button = Button(".one-sidebar-toolbar-button .oneicon-cart")
 
     spaces_header_list = WebItemsSequence(
-        ".sidebar-spaces li.one-list-item.clickable.resource-item"
-        " .item-header:not(.truncate)",
-        cls=Space,
+        ".sidebar-spaces li.one-list-item.clickable.resource-item", cls=Space
     )
     spaces_header_list_web_elems = WebElementsSequence(
         ".sidebar-spaces li.one-list-item.clickable.data-row"
