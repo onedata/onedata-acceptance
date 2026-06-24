3e070125669103f466c11a0d282d99aff4395549
Mateusz Zając <magros.thornrime@gmail.com>
2026-06-24 20:09:40 +0200

    VFS-13618 Remove redundant type alias

--- tests/gui/types.py
+++ tests/gui/types.py
@@ -32,7 +32,6 @@ class DynamicObject(Protocol):
     def __contains__(self, item: object) -> bool: ...
 
 
-type dict[str, str] = dict[str, str]
 type DriverMap = dict[str, WebDriver]
 type Numerals = dict[str, int]
 type TmpMemory = defaultdict[str, dict[str, DynamicObject]]
