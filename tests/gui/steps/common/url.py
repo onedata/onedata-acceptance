3bea39da454c0e78f7d66b7babfc13bd6263e912
Jakub-Karczewski <karczewskij@student.agh.edu.pl>
2026-06-24 23:58:46 +0200

    VFS-13618 Fixed comparison in getting node number, comparison between str and int

--- tests/gui/steps/common/url.py
+++ tests/gui/steps/common/url.py
@@ -73,7 +73,9 @@ def open_onedata_service_page(
         if "panel" in service:
             hostname = hosts[alias]["panel"]["hostname"]
 
-            if node_number != "":
+            if node_number == 0:
+                driver.get(f"https://{hostname}")
+            elif node_number != "":
                 driver.get(f"https://{hostname.split('.')[0]}-{node_number}.{hostname}")
             else:
                 driver.get(f"https://{hostname}")
