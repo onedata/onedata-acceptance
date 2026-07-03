97fb142d73bfe947f303fa3d5971355fd2bec767
Jakub-Karczewski <karczewskij@student.agh.edu.pl>
2026-07-03 21:21:33 +0200

    VFS-13656 Minor reformat

--- tests/gui/meta_steps/onezone/common.py
+++ tests/gui/meta_steps/onezone/common.py
@@ -311,7 +311,12 @@ def change_password(
     users[username].password = new_password
 
 
-@wt(parsers.parse('user of {browser_id} logins as "{username}" without closing authentication info alert'))
+@wt(
+    parsers.parse(
+        'user of {browser_id} logins as "{username}" without closing authentication'
+        " info alert"
+    )
+)
 def wt_sign_in_to_onezone_as_user(
     selenium: SeleniumDrivers,
     browser_id: str,
