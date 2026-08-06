be5b9f3c6b2a5a8745ae1aab447d322c0edb772a
Jakub-Karczewski <karczewskij@student.agh.edu.pl>
2026-08-06 22:08:34 +0200

    VFS-13752 Minor type-checking fix

--- tests/utils/rest_utils.py
+++ tests/utils/rest_utils.py
@@ -261,3 +261,5 @@ def http_request(  # pylint: disable=inconsistent-return-statements
             print("Test will freeze to allow debugging!")
             while True:
                 time.sleep(365 * 24 * 60 * 60)
+
+    raise RuntimeError("unreachable")
