d5903bff5ec15f1d5dfe1b8edb6f259df98457ce
Jakub-Karczewski <karczewskij@student.agh.edu.pl>
2026-08-27 17:40:50 +0200

    VFS-13591 Fix type-checker/merge error

--- tests/gui/utils/common/popups/query_builder.py
+++ tests/gui/utils/common/popups/query_builder.py
@@ -7,7 +7,7 @@ __copyright__ = "Copyright (C) 2020 ACK CYFRONET AGH"
 __license__ = "This software is released under the MIT license cited in LICENSE.txt"
 
 
-from tests.gui.conftest import WAIT_FRONTEND
+from tests.gui.utils.common.constants import WAIT_FRONTEND
 from tests.gui.utils.core.base import PageObject
 from tests.gui.utils.core.web_elements import Button, Input, Label, WebItemsSequence
 from tests.gui.utils.core.web_objects import PageObjectNotFoundError
