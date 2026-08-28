e08fed69ff48b4196e815bd98b02ed6b78fdd79c
Mateusz Zając <magros.thornrime@gmail.com>
2026-08-28 19:12:03 +0200

    VFS-13662 Fix clicking on space in menu list, update ruff rules

--- tests/gui/steps/onezone/spaces.py
+++ tests/gui/steps/onezone/spaces.py
@@ -67,7 +67,7 @@ def click_on_space_in_menu_list(driver: WebDriver, name: str, force: bool = True
     # function assumes data page is active
     page = OZLoggedIn(driver).data
     if force:
-        page.spaces_headers_list[name]()
+        page.spaces_headers_list[name].web_elem.click()
     elif not page.spaces_list[name].is_active():
         page.spaces_headers_list[name].click()
     return page
