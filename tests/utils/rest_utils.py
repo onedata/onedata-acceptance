9c9deae05347a2e56a026f24b515c7ead03db65a
Mateusz Zając <magros.thornrime@gmail.com>
2026-09-07 17:54:04 +0200

    VFS-13662 Explicitly set soft line length limit in format-check

--- tests/utils/rest_utils.py
+++ tests/utils/rest_utils.py
@@ -255,7 +255,7 @@ def http_request(
             |  __  |  | |     | |  |  ___/        | |      / /\ \ | |    | |            |  __  | |  | | . ` | | |_ |  | | | |
             | |  | |  | |     | |  | |            | |____ / ____ \| |____| |____        | |  | | |__| | |\  | |__| |  |_|_|_|
             |_|  |_|  |_|     |_|  |_|             \_____/_/    \_\______|______|       |_|  |_|\____/|_| \_|\_____/  (_|_|_)
-            """)  # noqa: E501, W291 - preserve the diagnostic ASCII banner
+            """)  # noqa: W291 - preserve the diagnostic ASCII banner
             traceback.print_stack()
             print("Test will freeze to allow debugging!")
             while True:
