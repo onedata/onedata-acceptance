f1695d3c859133a3060e5413cc175d56d32d8e70
Jakub-Karczewski <karczewskij@student.agh.edu.pl>
2026-09-11 22:38:28 +0200

    VFS-13798 Fix testrun by adding anootations from __future__

--- test_run.py
+++ test_run.py
@@ -7,6 +7,8 @@ All paths used are relative to script's path, not to the running user's CWD.
 Run the script with -h flag to learn about script's running options.
 """
 
+from __future__ import annotations
+
 import re
 import os
 import sys
